01eaf5f4ff415b13581f449bcec694e7da517202
haquocdat543 <wwwdatha543@gmail.com>
2023-12-07 13:20:49 +0000

    Update server main.tf

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -68,19 +68,29 @@ resource "aws_security_group" "JenkinsSecurityGroup" {
   description = "Allow SSH ,HTTPS , Jenkins, Sonarqube, React"
   vpc_id      = aws_vpc.jenkins-vpc.id
 
-  ingress = [
-    for port in [22, 80, 443, 8080, 9000] : {
-      description      = "TLS from VPC"
-      from_port        = port
-      to_port          = port
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    }
-  ]
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 9000
+    to_port          = 9000
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
